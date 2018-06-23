cd9f071f0a1c43fc1c72607ccab5e928f43fb222
TruongNM <manhtruonghedspi@gmail.com>
2018-06-23 17:07:00 +0700

    2.4 Create user profile page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def avatar_url user
+    return user.image.gsub!("_normal", "") if user.image
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "https://www.gravatar.com/avatar/#{gravatar_id}.jpg"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
     path: '',
     path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
     controllers: {omniauth_callbacks: 'omniauth_callbacks'}
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # get '/users/:id', to: 'users#show'
+  # /users/3 -> Users controller, show action, params {id: '3'}
+  resources :users, only: [:show]
 end
