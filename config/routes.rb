bf06f9cfd285bb94a6bc6ebbf6f882391dd1ebe5
TruongNM <manhtruonghedspi@gmail.com>
2018-07-01 07:50:54 +0700

    4.1 Like view

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
 
   resources :posts, only: [:index, :show, :create, :destroy] do
     resources :photos, only: [:create]
-    resources :likes, only: [:create, :destroy]
+    resources :likes, only: [:create, :destroy], shallow: true
   end
 end
