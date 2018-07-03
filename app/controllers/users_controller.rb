2a6b17651a262f527896cf710c0896ff4ea3c9bb
TruongNM <manhtruonghedspi@gmail.com>
2018-07-03 19:00:44 +0700

    4.10 Update user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @posts = @user.posts.includes(:photos, :likes, :comments)
+    @saved = Post.joins(:bookmarks).where(user_id: current_user.id).
+      includes(:photos, :likes, :comments) if @user == current_user
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   has_many :photos, dependent: :destroy
   has_many :likes, -> {order(:created_at => :desc)}
   has_many :comments, -> {order(:created_at => :desc)}
+  has_many :bookmarks
 
   def is_belongs_to? user
     Post.find_by(user_id: user.id, id: id)
