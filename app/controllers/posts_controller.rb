3784408c9aa5b2c3e0d54170e1a2d66199c310b3
TruongNM <manhtruonghedspi@gmail.com>
2018-07-05 19:14:32 +0700

    6.1 Add links, add pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,8 @@ class PostsController < ApplicationController
   before_action :find_post, only: [:show, :destroy]
 
   def index
-    @posts = Post.all.limit(10).includes(:photos, :user, :likes).
-      order('created_at desc')
+    @posts = Post.paginate(:page => params[:page], :per_page => 5).includes(:photos, :user, :likes).
+      order("created_at desc")
     @post = Post.new
   end
 
