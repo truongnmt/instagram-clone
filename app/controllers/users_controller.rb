f1e84039e5114e7aaa05e7a01c10bd980bc714f2
TruongNM <manhtruonghedspi@gmail.com>
2018-07-08 08:27:07 +0700

    Fix error Post join bookmark query

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     set_meta_tags title: @user.name
     @posts = @user.posts.includes(:photos, :likes, :comments)
-    @saved = Post.joins(:bookmarks).where(user_id: current_user.id).
+    @saved = Post.joins(:bookmarks).where("bookmarks.user_id=?", current_user.id).
       includes(:photos, :likes, :comments) if @user == current_user
   end
 end
