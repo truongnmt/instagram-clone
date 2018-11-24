637551eac4c24bf73279970c2b131bff3e3e4ba0
TruongNM <manhtruonghedspi@gmail.com>
2018-11-24 15:45:18 +0900

    6.2 Fix like text, authentication on user page, error post join bookmark query

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @users = User.search(params[:term])
     respond_to :js
@@ -7,7 +9,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.includes(:photos, :likes, :comments)
-    @saved = Post.joins(:bookmarks).where(user_id: current_user.id).
+    @saved = Post.joins(:bookmarks).where("bookmarks.user_id=?", current_user.id).
       includes(:photos, :likes, :comments) if @user == current_user
   end
 end
