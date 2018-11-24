981d6797c1e4db4e1191a072aff3c4ba361aad3a
TruongNM <manhtruonghedspi@gmail.com>
2018-11-24 16:06:46 +0900

    Fix foreign key for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :photos, dependent: :destroy
-  has_many :likes, -> {order(:created_at => :desc)}
-  has_many :comments, -> {order(:created_at => :desc)}
-  has_many :bookmarks
+  has_many :likes, -> {order(:created_at => :desc)}, dependent: :destroy
+  has_many :comments, -> {order(:created_at => :desc)}, dependent: :destroy
+  has_many :bookmarks, dependent: :destroy
 
   def is_belongs_to? user
     Post.find_by(user_id: user.id, id: id)
